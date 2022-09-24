13d8b8ff9a769761feecc785cbc03c74bbe3c312
павел <@DESKTOP-SO04HOV>
2022-09-24 21:28:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// КИПРОВ ПАВЕЛ 22 ИСП 2/2 ВАРИАНТ 15
+try
+{
+    Console.WriteLine("введите x");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    double x =double.Parse(Console.ReadLine());
+    double res = 2 * (1/Math.Tan((3 * x)) - 1) / (12 * x * x) + (7 * x) - 5;
+    Console.WriteLine($"{res:F2}");
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
